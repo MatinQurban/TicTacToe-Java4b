a20ad709851575277173e0c320059012e615965d
MatinQurban <matinqurban@gmail.com>
2025-01-29 14:12:18 -0800

    added SwitchMenu function (non-modular version) using existing SwitchToSinglePlayer function

--- src/main/java/com/java4b/tictactoe/MainMenuController.java
+++ src/main/java/com/java4b/tictactoe/MainMenuController.java
@@ -4,9 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+
 
 import java.io.IOException;
 
@@ -31,5 +34,24 @@ public class MainMenuController {
         Scene singleplayerScene = new Scene(root);
         Stage stage = (Stage) singlePlayer.getScene().getWindow();
         stage.setScene(singleplayerScene);
+    }
+
+    @FXML
+    protected void SwitchMenu(MouseEvent event) throws IOException {
+        // Get the source of which menu button was clicked
+        String menuID = ((Button) event.getSource()).getId();
+
+        switch(menuID){
+            case "SP":
+                SwitchToSinglePlayer();
+                break;
+            case "LM":
+                //SwitchToLocalMult();
+                break;
+            case "OM":
+                //SwitchToOnlineMult();
+                break;
+        }
+
     }
 }
