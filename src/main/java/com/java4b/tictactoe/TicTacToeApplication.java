6976018506fb94f474acde232a2fbcd7a2b294e6
MatinQurban <matinqurban@gmail.com>
2025-02-03 16:07:00 -0800

    added public switchScene helper function

--- src/main/java/com/java4b/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/java4b/tictactoe/TicTacToeApplication.java
@@ -1,6 +1,7 @@
 package com.java4b.tictactoe;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,17 +12,21 @@ import java.io.IOException;
 public class TicTacToeApplication extends Application {
     private static Scene scene;
 
-    public static Parent loadFXML(String fxml) throws IOException {
+    private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeApplication.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+    @FXML
+    public static void switchScene(String fxml, Stage stage) throws IOException {
+        Parent root = loadFXML(fxml);
+        Scene gameScene = new Scene(root);
+        stage.setScene(gameScene);
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = loadFXML("MainMenu");
-        scene = new Scene(root);
-        stage.setTitle("Tic Tac Toe");
-        stage.setScene(scene);
+        switchScene("MainMenu", stage);
         stage.show();
     }
 
