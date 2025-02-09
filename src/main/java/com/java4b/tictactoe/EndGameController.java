3a6c7fb17dad1794dd04c8a81e74d6653697d81d
Kelly Dempster <kellymdempster@gmail.com>
2025-02-08 23:36:03 -0800

    Updated controller functionality to switch scenes

---
+++ src/main/java/com/java4b/tictactoe/EndGameController.java
@@ -0,0 +1,52 @@
+package com.java4b.tictactoe;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EndGameController {
+
+    @FXML
+    private Label winnerText;
+    @FXML
+    private Button PA;
+    @FXML
+    private Button RTM;
+
+
+    @FXML
+    protected void switchMenu(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) winnerText.getScene().getWindow();
+
+        // Get the source of which menu button was clicked
+        String menuID = ((Button) event.getSource()).getId();
+
+        // Based on which menu button was clicked, switch to that corresponding fxml file
+        switch(menuID){
+            case "PA":
+                TicTacToeApplication.switchScene("single-player-game-view", stage);
+                break;
+            case "RTM":
+                TicTacToeApplication.switchScene("MainMenu", stage);
+                break;
+        }
+    }
+
+    @FXML
+    private void displayWinnerText(/* winner parameter */)
+    {
+        // logic should switch the text based on who won
+        winnerText.setText("Player X Won");
+
+        // case X, O, TIE
+
+    }
+
+
+
+}
