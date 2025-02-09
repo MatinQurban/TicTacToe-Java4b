50b351476732b3d05cfc40b70d4fcb62934b303e
Kehiio <kellymdempster@gmail.com>
2025-02-08 23:09:52 -0800

    end game controller creation

---
+++ src/main/java/com/java4b/tictactoe/endGameController.java
@@ -0,0 +1,22 @@
+package com.java4b.tictactoe;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+public class endGameController {
+
+   // @FXML
+
+    @FXML
+    protected void switchToMenu(ActionEvent event) throws IOException {
+        //Stage stage = (Stage).getScene().getWindow();
+        //TicTacToeApplication.switchScene("MainMenu", stage);
+    }
+
+
+
+}
