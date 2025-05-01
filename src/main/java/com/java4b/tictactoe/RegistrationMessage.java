7dc9ba654e7312509ca24265ba2c9c558f318540
christopherlewis <christophlewis7@gmail.com>
2025-04-30 18:48:24 -0700

    added files from chris' client-server-test

---
+++ src/main/java/com/java4b/tictactoe/RegistrationMessage.java
@@ -0,0 +1,20 @@
+package com.java4b.tictactoe;
+
+public class RegistrationMessage extends Message {
+    private static final long serialVerisionUID = 1L;
+    private final String channelToRegister;
+
+    public RegistrationMessage(String channelToRegister) {
+        super("/sys", "REGISTRATION");
+        this.channelToRegister = channelToRegister;
+    }
+
+    public String getChannelToRegister() {
+        return channelToRegister;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nChannel to Register: " + channelToRegister;
+    }
+}
